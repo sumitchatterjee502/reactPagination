b47fe6c1ac4e837f051e0a5cd5bc354067732163
sumitchatterjee <53476831+sumitchatterjee502@users.noreply.github.com>
2022-07-27 00:39:35 +0530

    Update and rename Table.jsx to Example.jsx

---
+++ Example.jsx
@@ -0,0 +1,46 @@
+import React,{useState, useEffect} from 'react';
+import {Link} from 'react-router-dom';
+import {Button} from 'react-bootstrap';
+import PaginationCustom from '../Library/Pagination';
+
+const Table = (props)=> {
+
+    const tableList= props.tableList;
+    const pageSize = props.pageSize;
+    
+    const [paginatedPosts, setPaginatedPosts] = useState();
+    const [currentPage, setCurrentPage] = useState(1);
+
+    useEffect(() => {
+        if(tableList !== ''){
+            const firstPageIndex = (currentPage - 1) * pageSize;
+            const lastPageIndex = firstPageIndex + pageSize;
+            setPaginatedPosts(tableList.slice(firstPageIndex,lastPageIndex)); 
+            //setPaginatedPosts(_(tableList).slice(startIndex).take(pageSize).value());       
+        }else{
+            setPaginatedPosts('')
+        }
+
+    }, [tableList, pageSize, currentPage]);
+
+    const totalCount = tableList ? tableList.length : 0 ;
+    
+    
+    return (
+        <React.Fragment>
+            
+            <nav className='d-flex justify-content-center'>
+
+                <PaginationCustom
+                    className="pagination-bar"
+                    currentPage={currentPage}
+                    totalCount={totalCount}
+                    pageSize={pageSize}
+                    onPageChange={page => setCurrentPage(page)}
+                />
+            </nav>
+        </React.Fragment>
+    )
+}
+
+export default Table
